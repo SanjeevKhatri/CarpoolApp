a2ae08c7e1aa72482a2a3a6d47c77482e52826ac
khatr <khatr@10.10.13.31>
2016-12-20 12:00:38 -0600

    controller

--- src/edu/mum/project/controller/LoginController.java
+++ src/edu/mum/project/controller/LoginController.java
@@ -9,12 +9,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.mum.project.serviceImpl.UserServiceImpl;
+
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher view = request.getRequestDispatcher("view/login.html");
-		view.forward(request, response);
+
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		
+		UserServiceImpl usi= new UserServiceImpl();
+		try {
+			if(usi.checkUserLogin(email, password)==true){
+				RequestDispatcher view = request.getRequestDispatcher("view/feed.html");
+				view.forward(request, response);				
+			}
+			else{
+				RequestDispatcher view = request.getRequestDispatcher("view/index.html");
+				view.forward(request, response);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
 	}
 }
