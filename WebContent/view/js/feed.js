c487d99b3bcda08d53117dd70abd205c51c04ee4
gurkhaz <sanjayshrestha31@gmail.com>
2016-12-20 21:34:35 -0600

    notification maintaining

--- WebContent/view/js/feed.js
+++ WebContent/view/js/feed.js
@@ -1,3 +1,5 @@
+var old_count=0;
+
 $(document).ready(function () {
 	$.ajax({
 	url:'DisplayPost',
@@ -26,16 +28,31 @@ $(document).ready(function () {
 });
 	
 	
-	$.ajax({
-		url:'PostController',
-		type:'GET',
-		headers:{
-			Accept:"application/json; charset=utf-8",
-			"Content-Type" : "application/json; charset=utf-8"
-		},
-		success:function(result){
-			alert(result.JSONDATAC);
-		}
-	});
 	
+	// ajax call at certain interval
+	
+	setInterval(function () {
+        console.log('it works' + new Date());
+	
+	
+					$.ajax({
+						url:'PostController',
+						type:'GET',
+						headers:{
+							Accept:"application/json; charset=utf-8",
+							"Content-Type" : "application/json; charset=utf-8"
+						},
+						success:function(result){
+							
+							var new_count = result.JSONDATAC;
+							if(new_count>old_count){
+								
+								 $(".inner-top-link > li > a.notification").css("background-color","red");
+							}
+							console.log(result.JSONDATAC);
+						}
+					});
+					
+	 },3000);
+					
 });
